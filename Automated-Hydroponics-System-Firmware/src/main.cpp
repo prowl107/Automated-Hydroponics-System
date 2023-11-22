fd6f6a4e226c841943f081997a1a2ce08f34c8b7
prowl107 <milesosborne182@gmail.com>
2023-11-22 12:10:43 -0500

    Added empty main function, does nothing

--- Automated-Hydroponics-System-Firmware/src/main.cpp
+++ Automated-Hydroponics-System-Firmware/src/main.cpp
@@ -1,19 +1,9 @@
 #include <avr/io.h>
+
 #define F_CPU 
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main()
+{
+  /* Insert code here :) */
+  return 0;
 }
